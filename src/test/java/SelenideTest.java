966c64469c208c8c2dcb85c75ca3c1045b0b9ed1
Александр Масляков <lapostov@yandex.ru>
2022-12-10 11:31:26 +0400

    Corrected test

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -2,8 +2,11 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,18 +14,30 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
+    public String generateDate(int days) {
+
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    }
+
+    String planningDate = generateDate(3);
+
     @Test
     public void test() {
         open("http://localhost:9999");
         $("input[placeholder='Город']").setValue("Владивосток");
-        $("input[placeholder='Дата встречи']").setValue("06.12.2022");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("input[name='name']").setValue("Александр Сергеевич");
         $("input[name='phone']").setValue("+79271620864");
         $x("//span[@class='checkbox__box']").click();
         $x("//span[@class='button__text']").click();
         String text = $x("//div[@class='notification__content']").should(visible, Duration.ofSeconds(15)).getText();
-        Assertions.assertEquals("Встреча успешно забронирована на 09.12.2022", text.trim());
+        $(".notification__content")
 
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+
+                .shouldBe(Condition.visible);
 
 
     }
